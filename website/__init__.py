f065fc52839b67bfc706706b83eeaf88ef112b32
dzikr <dzikrikovalesky12@gmail.com>
2022-10-26 13:41:11 +0700

    Finish to create note application web

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,34 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+from .models import User
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+migrate = Migrate()
 
 
 def create_app():
-    app = Flask(__name__, template_folder='templates')
-    app.config['SECRET_KEY'] = 'awsad'
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
