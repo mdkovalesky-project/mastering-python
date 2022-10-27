c2efa47e2cc24ae36e51966bda33ecbaa51f6473
dzikr <dzikrikovalesky12@gmail.com>
2022-10-27 17:19:17 +0700

    api view

--- website/api/views.py
+++ website/api/views.py
@@ -1,11 +1,25 @@
-from flask_restx import Resource
-from flask import jsonify
+from flask_restx import Resource, Api, reqparse
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+api = Api(app, title='Nodium', description='Start from Reader Become Writer')
+
+ns = api.models('Nodium', description='Nodium operation')
+
 
 class NotesView(Resource):
     def get(self):
         return jsonify(
             {
                 "message": "testing API",
-                "message2": "Testing API 2q"
+                "message2": "Testing API 2"
             }
         )
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', action='append')
+        parser.add_argument('article', action='append')
+
+        
+
