9dbf8e142478b113fab23ec45f009e90d8376765
dzikr <dzikrikovalesky12@gmail.com>
2022-10-28 16:40:52 +0700

    view error 3

--- website/views.py
+++ website/views.py
@@ -19,7 +19,7 @@ def home():
             new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Note added now!', category='success')
 
     return render_template("home.html", user=current_user)
 
